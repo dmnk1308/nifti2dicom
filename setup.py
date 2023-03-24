363b23efc24b73da0c0563abe46bf0f4d194850f
dmnk1308 <dominikbe@aol.com>
2023-03-24 09:59:11 +0100

    update modality release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
   
 setup(
     name='nifti2dicom',
-    version='0.1',
+    version='0.1.1',
     description='Package translate nifti to dicom files',
     author='Dominik Becker',
     author_email='dominik.becker1@uni-goettingen.de',
